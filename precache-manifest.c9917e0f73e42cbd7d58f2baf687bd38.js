397af91b6ca4538e7c023d45cba55fb31e573e2a
DanielMendesdoAmaral <daniel.amaral720@gmail.com>
2021-03-09 14:47:35 -0300

    Updates

---
+++ precache-manifest.c9917e0f73e42cbd7d58f2baf687bd38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030de2cc215e8b310d3f6124bec9d355",
+    "url": "/pwa-react-teste/index.html"
+  },
+  {
+    "revision": "52a10f9f51c3b155ea72",
+    "url": "/pwa-react-teste/static/css/main.04a8df01.chunk.css"
+  },
+  {
+    "revision": "6a8580d22f4933fbc3ae",
+    "url": "/pwa-react-teste/static/js/2.726216ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pwa-react-teste/static/js/2.726216ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52a10f9f51c3b155ea72",
+    "url": "/pwa-react-teste/static/js/main.a4729bb9.chunk.js"
+  },
+  {
+    "revision": "53220c2751b81663285c",
+    "url": "/pwa-react-teste/static/js/runtime-main.372cedaf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-react-teste/static/media/logo.5d5d9eef.svg"
+  }
+]);
